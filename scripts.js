4e40b6ad4a27c31ea7c208c9846dd1bff1cb808a
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-06-27 13:57:02 -0400

    Buttons

--- scripts.js
+++ scripts.js
@@ -2,3 +2,21 @@
 var lat: =
 var long: =
 */
+
+console.log("Omar Button Clicked");
+var button = document.getElementById("Omar");
+button.addEventListener("click", function() {
+    alert("Hi you clicked Omar's Button!");
+});
+
+console.log("Wayil Button Clicked");
+var button = document.getElementById("Wayil");
+button.addEventListener("click", function() {
+    alert("Hi you clicked Wayil's Button!");
+});
+
+console.log("Datesta Button Clicked");
+var button = document.getElementById("Datesta");
+button.addEventListener("click", function() {
+    alert("Hi you clicked Datesta's Button!");
+});
