84dd0dde7cdbca1510bf0e16326cf6335eedef22
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-07-01 23:01:19 -0400

    Fixed Most of Buttons
    Datesta's Button for some reason won't work though.

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
     //Omars Park Button
     var OmarParkButton = document.getElementById("OmarParkButton");
     console.log(OmarParkButton);
-    OmarButton.addEventListener("click", function() {
+    OmarParkButton.addEventListener("click", function() {
         console.log("Omar's Park Button Clicked Button Clicked");
         alert("Hi you clicked Omar's Park Button!");
         map.setCenter(new google.maps.LatLng(41.0382745, -73.5464977));
@@ -36,23 +36,63 @@ $(document).ready(function() {
     });  
    
     //  ================================ Wayils Buttons ======================================
-    //Wayils Button Park Button
-    var WayilButton = document.getElementById("WayilPark");
-    console.log(WayilButton);
-    WayilButton.addEventListener("click", function() {
-        console.log("Wayil Button Clicked");
-        alert("Hi you clicked Wayil's Button!");
+    //Wayils Restaurants Button
+    var WayilRestaurantButton = document.getElementById("WayilRestaurantButton");
+    console.log(WayilRestaurantButton);
+    WayilRestaurantButton.addEventListener("click", function() {
+        console.log("Wayil's Restaraunt Button Clicked");
+        alert("Hi you clicked Wayil's Restaurants Button!");
+        map.setCenter(new google.maps.LatLng(-34.397, 150.644));
+        map.setZoom(15);
+    });
+
+    //Wayils Parks Button
+    var WayilParkButton = document.getElementById("WayilParkButton");
+    console.log(WayilParkButton);
+    WayilParkButton.addEventListener("click", function() {
+        console.log("Wayil's Parks Button Clicked");
+        alert("Hi you clicked Wayil's Parks Button!");
         map.setCenter(new google.maps.LatLng(-34.397, 150.644));
         map.setZoom(15);
     });
     
-    //  =============================== Datesta's Bittpms ====================================
-    //Datesta's Park Button
-    var DatestaButton = document.getElementById("DatestaPark");
-    console.log(DatestaButton);
-    DatestaButton.addEventListener("click", function() {
-        console.log("Datesta Button Clicked");
-        alert("Hi you clicked Datesta's Button!");
+    //Wayils Stores Button
+    var WayilStoreButton = document.getElementById("WayilStoreButton");
+    console.log(WayilStoreButton);
+    WayilStoreButton.addEventListener("click", function() {
+        console.log("Wayil Store Button Clicked");
+        alert("Hi you clicked Wayil's Stores Button!");
+        map.setCenter(new google.maps.LatLng(-34.397, 150.644));
+        map.setZoom(15);
+    });
+     
+    //  =============================== Datesta's Buttons ====================================
+    //Datesta's Restarants Button
+    var DatestaRestaurantButton = document.getElementById("DatestaRestarauntButton");
+    console.log(DatestaRestaruantButton);
+    DatestaRestaurantButton.addEventListener("click", function() {
+        console.log("Datesta's Restaruants Button Clicked");
+        alert("Hi you clicked Datesta's Restaraunts Button!");
+        map.setCenter(new google.maps.LatLng(-41.1792, 73.1894));
+        map.setZoom(15);
+    });
+
+    //Datesta's Parks Button
+    var DatestaParkButton = document.getElementById("DatestaParkButton");
+    console.log(DatestaParkButton);
+    DatestaParkButton.addEventListener("click", function() {
+        console.log("Datesta's Parks Button Clicked");
+        alert("Hi you clicked Datesta's Parks Button!");
+        map.setCenter(new google.maps.LatLng(-41.1792, 73.1894));
+        map.setZoom(15);
+    });
+
+    //Datesta's Stores Button
+    var DatestaStoreButton = document.getElementById("DatestaStoreButton");
+    console.log(DatestaStoreButton);
+    DatestaStoreButton.addEventListener("click", function() {
+        console.log("Datesta's Stores Button Clicked");
+        alert("Hi you clicked Datesta's Stores Button!");
         map.setCenter(new google.maps.LatLng(-41.1792, 73.1894));
         map.setZoom(15);
     });
