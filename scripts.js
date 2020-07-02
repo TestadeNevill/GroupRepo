ddc7bbd212996ef627b73e18d01296ad7f748e0c
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-07-01 22:29:10 -0400

    Dropdown Menus Navbar
    Added Background Image for NavBar as well.

--- scripts.js
+++ scripts.js
@@ -4,18 +4,40 @@ var long: =
 */
 $(document).ready(function() {
 
-    //Omars Button (Add dropdown menu)
-    var OmarButton = document.getElementById("Omar");
-    console.log(OmarButton);
+    //  ================================= Omars Buttons ======================================
+    //Omars Restaurant Button
+    var OmarRestaurantButton = document.getElementById("OmarRestaurantButton");
+    console.log(OmarRestaurantButton);
+    OmarRestaurantButton.addEventListener("click", function() {
+        console.log("Omar's Restaurant Button Clicked!");
+        alert("Hi you clicked Omar's Restaurant Button!");
+        map.setCenter(new google.maps.LatLng(41.0382745, -73.5464977));
+        map.setZoom(15);
+    });  
+
+    //Omars Park Button
+    var OmarParkButton = document.getElementById("OmarParkButton");
+    console.log(OmarParkButton);
     OmarButton.addEventListener("click", function() {
-        console.log("Omar Button Clicked");
-        alert("Hi you clicked Omar's Button!");
+        console.log("Omar's Park Button Clicked Button Clicked");
+        alert("Hi you clicked Omar's Park Button!");
+        map.setCenter(new google.maps.LatLng(41.0382745, -73.5464977));
+        map.setZoom(15);
+    });  
+
+    //Omars Store Button
+    var OmarStoreButton = document.getElementById("OmarStoreButton");
+    console.log(OmarStoreButton);
+    OmarStoreButton.addEventListener("click", function() {
+        console.log("Omar's Store Button Clicked!");
+        alert("Hi you clicked Omar's Store Button!");
         map.setCenter(new google.maps.LatLng(41.0382745, -73.5464977));
         map.setZoom(15);
     });  
    
-    //Wayils Button (Add dropdown menu)
-    var WayilButton = document.getElementById("Wayil");
+    //  ================================ Wayils Buttons ======================================
+    //Wayils Button Park Button
+    var WayilButton = document.getElementById("WayilPark");
     console.log(WayilButton);
     WayilButton.addEventListener("click", function() {
         console.log("Wayil Button Clicked");
@@ -24,8 +46,9 @@ $(document).ready(function() {
         map.setZoom(15);
     });
     
-    //Datesta's Button (Add dropdown menu)
-    var DatestaButton = document.getElementById("Datesta");
+    //  =============================== Datesta's Bittpms ====================================
+    //Datesta's Park Button
+    var DatestaButton = document.getElementById("DatestaPark");
     console.log(DatestaButton);
     DatestaButton.addEventListener("click", function() {
         console.log("Datesta Button Clicked");
